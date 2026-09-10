2326c3ab04b15f99d061a7289ef1cc03e64c97a4
hariniinakollu <hariniinakollu@gmail.com>
2026-09-10 20:39:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+app.secret_key = "cineverse_secret_key"
+
+
+# LOGIN PAGE
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == "admin" and password == "1234":
+            session["logged_in"] = True
+            return redirect(url_for("home"))
+        else:
+            return render_template("login.html", error="Invalid username or password!")
+
+    return render_template("login.html")
+
+
+# HOME PAGE
+@app.route("/")
+def home():
+    if "logged_in" not in session:
+        return redirect(url_for("login"))
+
+    return render_template("index.html")
+
+
+# BOOKING PAGE
+@app.route("/booking")
+def booking():
+    if "logged_in" not in session:
+        return redirect(url_for("login"))
+
+    return render_template("booking.html")
+
+
+# PAYMENT PAGE
+@app.route("/payment")
+def payment():
+    if "logged_in" not in session:
+        return redirect(url_for("login"))
+
+    return render_template("payment.html")
+
+
+# LOGOUT
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
